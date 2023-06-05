27427eaeeabacb4815d5f67dc0cf7ec06e85dd46
Eric <Eric>
2023-06-05 17:37:08 +0800

    feature:透過composer 安裝swagger

--- app/Http/Controllers/Api/V1/Tests/TestController.php
+++ app/Http/Controllers/Api/V1/Tests/TestController.php
@@ -6,6 +6,15 @@ use App\Http\Controllers\Controller;
 
 class TestController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/users",
+     *     operationId="getUsersList",
+     *     tags={"Users"},
+     *     summary="Get list of users",
+     *     @OA\Response(response="200", description="Successful operation"),
+     * )
+     */
     public function index()
     {
         dd(auth('api')->user());
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         title="API 文檔",
+ *         version="1.0.0",
+ *         description="這是我的 API 文檔",
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
